105d8537f3b395ce2bc01dd5707b4f2581bc5c38
Liao Mingqing <liaomq1900127@163.com>
2019-08-25 16:05:08 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'ELASTIC3rd doc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'ELASTIC3rd.tex', u'ELASTIC3rd Documentation',
+   u'ELASTIC3rd Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'ELASTIC3rd', u'ELASTIC3rd Documentation',
+     [u'Manual'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,9 +240,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
-   'Miscellaneous'),
+  ('index', 'ELASTIC3rd', u'ELASTIC3rd Documentation',
+   u'ELASTIC3rd Developers', 'ELASTIC3rd', 'ELASTIC3rd Project.',
+   'Physics, Mechanics'),
 ]
 
 # Documents to append as an appendix to all manuals.
