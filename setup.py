b911579ce6248a330e3928fd3cf1b2c0a9549e0c
Mingqing Liao <liaomq1900127@163.com>
2020-02-03 02:50:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,20 +9,26 @@ try:
 except ImportError :
     raise ImportError("Numpy and Scipy must be installed.")
 
-files = ["energy/energyrun"]
+#files_erun = ["energy/energyrun"]
 
 setup(
     name = "ELASTIC3rd",
-    version = "1.1",
-    description = "Claculate 3rd order elastic constant",
+    version = "2.4.2",
+    description = "Claculate 3rd order elastic constants for crystals",
     author = "Mingqing Liao",
     author_email = "liaomq1900127@163.com",
-    url = 'https://github.com/hitliaomq',
+    url = 'https://github.com/hitliaomq/ELASTIC3rd',
+    download_url = 'https://github.com/hitliaomq/ELASTIC3rd',
     license = "GPL3",
+    platforms = ['linux', 'windows'],
+    keywords = ['physics', 'materials', 'elastic constants'],
     packages = ['elastic3rd', 'elastic3rd.crystal', 'elastic3rd.energy', 'elastic3rd.post', 'elastic3rd.symmetry'],
+    package_dir = {'elastic3rd': 'src/elastic3rd', 'elastic3rd.energy' : 'src/elastic3rd/energy', 
+                   'elastic3rd.symmetry' : 'src/elastic3rd/symmetry', 'elastic3rd.crystal' : 'src/elastic3rd/crystal', 
+                   'elastic3rd.post' : 'src/elastic3rd/post'},
+    package_data = {'elastic3rd.energy' : ['energyrun']},
     install_requires = ['numpy', 'scipy', 'matplotlib'],
-    #long_description = read('README.md'),
-    package_data = {'elastic3rd' : files},
+    #long_description = read('README.md'),    
     classifiers = [
         "Development Status :: 4 - Beta",
         "Intended Audience :: Education",
